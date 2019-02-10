3779cded9027ecace531bc65f95e1d6fd8bace8d
Cristiane <cristiane@Eva.local>
2019-02-10 21:42:28 -0200

    alterando para ActionController::API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   rescue_from ActionController::ParameterMissing do
     render json: { message: 'invalid payload' }, status: :unprocessable_entity
   end
